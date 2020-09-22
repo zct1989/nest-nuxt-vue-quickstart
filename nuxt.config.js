d2edfd55428966c0593edf19a08a1b5eee689102
zhuchentong <2037630@163.com>
2020-09-22 18:33:33 +0800

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,42 +1,48 @@
 export default {
-  // Global page headers (https://go.nuxtjs.dev/config-head)
-  mode: 'universal',
-  srcDir: 'src/client/',
-  telemetry: false,
-  head: {
-    title: 'client',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+    // Global page headers (https://go.nuxtjs.dev/config-head)
+    mode: 'universal',
+    srcDir: 'src/client/',
+    telemetry: false,
+    head: {
+        title: 'client',
+        meta: [
+            { charset: 'utf-8' },
+            {
+                name: 'viewport',
+                content: 'width=device-width, initial-scale=1'
+            },
+            { hid: 'description', name: 'description', content: '' }
+        ],
+        link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    },
+
+    // Global CSS (https://go.nuxtjs.dev/config-css)
+    css: ['ant-design-vue/dist/antd.css'],
+
+    // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
+    plugins: [
+        { src: '@/plugins/antd-ui', ssr: true },
+        { src: '@/plugins/vue-grid-layout', ssr: false }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
-  },
 
-  // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: ['ant-design-vue/dist/antd.css'],
+    // Auto import components (https://go.nuxtjs.dev/config-components)
+    components: true,
 
-  // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: ['@/plugins/antd-ui'],
+    // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
+    buildModules: [
+        // https://go.nuxtjs.dev/typescript
+        '@nuxt/typescript-build'
+    ],
 
-  // Auto import components (https://go.nuxtjs.dev/config-components)
-  components: true,
+    // Modules (https://go.nuxtjs.dev/config-modules)
+    modules: [
+        // https://go.nuxtjs.dev/axios
+        '@nuxtjs/axios'
+    ],
 
-  // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
-  buildModules: [
-    // https://go.nuxtjs.dev/typescript
-    '@nuxt/typescript-build',
-  ],
+    // Axios module configuration (https://go.nuxtjs.dev/config-axios)
+    axios: {},
 
-  // Modules (https://go.nuxtjs.dev/config-modules)
-  modules: [
-    // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
-  ],
-
-  // Axios module configuration (https://go.nuxtjs.dev/config-axios)
-  axios: {},
-
-  // Build Configuration (https://go.nuxtjs.dev/config-build)
-  build: {},
-};
+    // Build Configuration (https://go.nuxtjs.dev/config-build)
+    build: {}
+}
--- src/client/pages/index.vue
+++ src/client/pages/index.vue
@@ -1,75 +1,73 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        client
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+    <div class="container">
+        <no-ssr>
+            <grid-layout
+                :layout.sync="layout"
+                :col-num="12"
+                :row-height="30"
+                :is-draggable="true"
+                :is-resizable="true"
+                :is-mirrored="false"
+                :vertical-compact="true"
+                :margin="[10, 10]"
+                :use-css-transforms="true"
+            >
+                <grid-item
+                    v-for="item in layout"
+                    :x="item.x"
+                    :y="item.y"
+                    :w="item.w"
+                    :h="item.h"
+                    :i="item.i"
+                    :key="item.i"
+                >
+                    <div class="drag-item">
+                        {{ item.i }}
+                    </div>
+                </grid-item>
+            </grid-layout>
+        </no-ssr>
     </div>
-  </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
-
-export default Vue.extend({})
+import { Component, Vue } from 'vue-property-decorator'
+@Component({
+    components: {}
+})
+export default class extends Vue {
+    private readonly layout = [
+        { x: 0, y: 0, w: 2, h: 2, i: '0' },
+        { x: 2, y: 0, w: 2, h: 4, i: '1' },
+        { x: 4, y: 0, w: 2, h: 5, i: '2' },
+        { x: 6, y: 0, w: 2, h: 3, i: '3' },
+        { x: 8, y: 0, w: 2, h: 3, i: '4' },
+        { x: 10, y: 0, w: 2, h: 3, i: '5' },
+        { x: 0, y: 5, w: 2, h: 5, i: '6' },
+        { x: 2, y: 5, w: 2, h: 5, i: '7' },
+        { x: 4, y: 5, w: 2, h: 5, i: '8' },
+        { x: 6, y: 3, w: 2, h: 4, i: '9' },
+        { x: 8, y: 4, w: 2, h: 4, i: '10' },
+        { x: 10, y: 4, w: 2, h: 4, i: '11' },
+        { x: 0, y: 10, w: 2, h: 5, i: '12' },
+        { x: 2, y: 10, w: 2, h: 5, i: '13' },
+        { x: 4, y: 8, w: 2, h: 4, i: '14' },
+        { x: 6, y: 8, w: 2, h: 4, i: '15' },
+        { x: 8, y: 10, w: 2, h: 5, i: '16' },
+        { x: 10, y: 4, w: 2, h: 2, i: '17' },
+        { x: 0, y: 9, w: 2, h: 3, i: '18' },
+        { x: 2, y: 6, w: 2, h: 2, i: '19' }
+    ]
+}
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+<style lang="less">
+.vue-grid-item {
+    background: red;
+    color: white;
+    font-size: 24px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 }
 </style>
